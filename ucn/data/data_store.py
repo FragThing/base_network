9b34dbfdbe6debaa85f54b4734fe8b28ae298e18
xz-dev <xiangzhedev@gmail.com>
2023-01-31 23:12:15 +0800

    add ucn data store network type

--- ucn/data/data_store.py
+++ ucn/data/data_store.py
@@ -3,24 +3,25 @@
 from ucn.url_parse import url_parse
 from ucn.data.store_type import StoreType
 from ucn.data.ucn_store import ucn_store
+from ucn.network.net_type import BaseType
 
 
 class DataStore:
     """Data store wrapper"""
 
-    def read(self, url: str) -> bytes or None:
+    def read(self, url: str, net_type: BaseType = None) -> bytes or None:
         """Read data"""
         scheme_list = url_parse.get_scheme(url)
         store_type = StoreType(scheme_list.pop())
         re_url = url_parse.reset_scheme(url, scheme_list)
         if store_type is StoreType.UCN:
-            return ucn_store.read(re_url)
+            return ucn_store.read(net_type, re_url)
         return None
 
-    def save(self, store_type: StoreType, content: bytes) -> str:
+    def save(self, store_type: StoreType, content: bytes, net_type: BaseType = None) -> str:
         """Save data from file"""
         if store_type is StoreType.UCN:
-            url = ucn_store.save(content)
+            url = ucn_store.save(net_type, content)
         scheme_list = url_parse.get_scheme(url)
         scheme_list.insert(0, store_type.value)
         return url_parse.reset_scheme(url, scheme_list)
--- ucn/data/ucn_store.py
+++ ucn/data/ucn_store.py
@@ -5,52 +5,52 @@ from ucn.data.base_local_store import base_local_store
 from ucn.url_parse import url_parse
 
 from ucn.network.network_wrap import send
-from ucn.network.net_type import DataStoreType
+from ucn.network.net_type import BaseType
 
 
 class UCNDataStore:
     """UCN Data store controller"""
 
-    def read(self, url) -> bytes or None:
+    def read(self, net_type: BaseType, url: str) -> bytes or None:
         """Read bytes content by URL string
         If URL check fail return None
         """
         content = self.__read_local(url)
         if not content:
-            content = self.__read_net(url)
+            content = self.__read_net(net_type, url)
         encode_algo = urlparse(url).scheme
         if url_parse.encode(content, encode_algo) == url:
             return content
         return None
 
-    def save(self, content: bytes) -> str:
+    def save(self, net_type: BaseType, content: bytes) -> str:
         """Save bytes content and return URL string"""
         url = url_parse.encode(content, force_hash=True)
         self.__save_local(url, content)
-        self.__save_net(url, content)
+        self.__save_net(net_type, url, content)
         return url
 
     def __read_local(self, url: str) -> bytes or None:
         """Read data in local"""
         try:
-            return base_local_store.read(base_local_store.FILE_STORE, url)
+            return base_local_store.read(url)
         except FileNotFoundError:
             return None
 
-    def __read_net(self, url: str) -> bytes or None:
+    def __read_net(self, net_type: BaseType, url: str) -> bytes or None:
         """Request data in ucn network"""
-        return send(DataStoreType.READ, url.encode("utf-8"))
+        return send(net_type, url.encode("utf-8"))
 
     def __save_local(self, url: str, data) -> bytes or None:
         """Save data in local"""
         try:
-            return base_local_store.save(data, base_local_store.FILE_STORE, url)
+            return base_local_store.save(data, url)
         except FileNotFoundError:
             return None
 
-    def __save_net(self, url: str, data) -> bytes or None:
+    def __save_net(self, net_type: BaseType, url: str, data) -> bytes or None:
         """Push data in ucn network"""
-        return send(DataStoreType.SAVE, url.encode("utf-8") + "\n" + data)
+        return send(net_type, url.encode("utf-8") + "\n" + data)
 
 
 ucn_store = UCNDataStore()
